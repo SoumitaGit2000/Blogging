affa7f6b66f4554f6d209c30ee302a3fe75dcfb2
Soumita Banerjee <soumitabanerjee2000@gmail.com>
2025-07-10 22:02:26 +0530

    connection add

--- connections/connection.js
+++ connections/connection.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose")
 mongoose.set("strictQuery", true)
 
+// async function connectmongoose (url) {
+//     return mongoose.connect(url)
+// }
 async function connectmongoose (url) {
-    return mongoose.connect(url)
-}
+     return mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 20000 // 20 seconds
+})
+ }
 
 module.exports = {
     connectmongoose
